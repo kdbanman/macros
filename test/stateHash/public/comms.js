79a1af55106b586b24f0296feaaa1e9ff50ca45a
kdbanman <kdbanman@ualberta.ca>
2014-06-17 17:10:50 -0600

    improve structure and add bulk dynamism

--- test/stateHash/public/comms.js
+++ test/stateHash/public/comms.js
@@ -1,9 +1,49 @@
 var socket = io(window.location.origin);
+socket.waiting = true;
 
 socket.on('generate', function (data) {
+
+    // set socket waiting state
+    socket.waiting = false;
     
-    var statusStr = 'GENERATING SIZE ' + data.size + ' FROM SEED ' + data.seed;
+    // change status and output divs for new command
+    
+    $('#rendered').html('');
+    $('#hashcode').html('');
+    
+    var statusStr = 'GENERATING OBJECT SIZE ' +
+                    data.size +
+                    'FROM SEED ' +
+                    data.seed;
     $('#status').html(statusStr);
 
-    socket.emit('result', data);
+    // generate object
+    
+    var generator = new MersenneTwister(data.seed);
+    var generated = rand.generateObject(data.size, generator);
+
+    // serialize and render object
+
+    $('#status').html('SERIALIZING OBJECT');
+    var serialized = JSON.stringify(generated, null, '  ');
+    data.object = serialized;
+
+    $('#rendered').html(serialized);
+
+    // hash object
+
+    $('#status').html('HASHING OBJECT');
+    var hashcode = esHash.hash(generated);
+    data.hashcode = hashcode;
+
+    $('#hashcode').html(hashcode);
+
+    // report to server
+    $('#status').html('REPORTING TO SERVER');
+    socket.emit('result', data, function () {
+        if (socket.waiting) $('#status').html('WAITING ON SERVER');
+    });
+
+    // set socket to waiting 
+    socket.waiting = true;
 });
