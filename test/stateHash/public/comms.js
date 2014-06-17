b62fa6f5e96e6bfd4854a6bb55dd892eff7ac4eb
kdbanman <kdbanman@ualberta.ca>
2014-06-17 15:18:31 -0600

    use jquery to show status

--- test/stateHash/public/comms.js
+++ test/stateHash/public/comms.js
@@ -1,7 +1,9 @@
 var socket = io(window.location.origin);
 
 socket.on('generate', function (data) {
-    console.log('received generate command');
-    console.log(data);
+    
+    var statusStr = 'GENERATING SIZE ' + data.size + ' FROM SEED ' + data.seed;
+    $('#status').html(statusStr);
+
     socket.emit('result', data);
 });
