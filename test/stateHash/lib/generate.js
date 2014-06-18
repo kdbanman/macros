d07a0af08c2783221bc31559751fd980300ae22b
kdbanman <kdbanman@ualberta.ca>
2014-06-17 20:11:34 -0600

    scale size with seed linearly.

--- test/stateHash/lib/generate.js
+++ test/stateHash/lib/generate.js
@@ -2,7 +2,7 @@
 // this ensures that a deterministic sequence of generate commands are sent
 // there will be N objects of each size generated
 module.exports = function (seed, N) {
-    N = N || 10;
+    N = N || 1;
     var size = Math.ceil(seed / N);
     return {size: size, seed: seed};
 };
