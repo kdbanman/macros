e35994c1668f105a95dcc682a7550239b400af42
kdbanman <kdbanman@ualberta.ca>
2014-06-17 18:45:34 -0600

    set size increase every 10 seeds

--- test/stateHash/lib/generate.js
+++ test/stateHash/lib/generate.js
@@ -1,7 +1,8 @@
-// each generate command size = ceil(currSeed / 100), seed = currSeed
+// each generate command size = ceil(currSeed / N), seed = currSeed
 // this ensures that a deterministic sequence of generate commands are sent
-// there will be 100 objects of each size generated
-module.exports = function (seed) {
-    var size = Math.ceil(seed / 100);
+// there will be N objects of each size generated
+module.exports = function (seed, N) {
+    N = N || 10;
+    var size = Math.ceil(seed / N);
     return {size: size, seed: seed};
 };
