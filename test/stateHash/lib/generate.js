d2b877f7cc5b20eb8420c3ca3583c946c4995e1b
kdbanman <kdbanman@ualberta.ca>
2014-06-17 22:06:11 -0600

    make size wrap around

--- test/stateHash/lib/generate.js
+++ test/stateHash/lib/generate.js
@@ -1,8 +1,6 @@
-// each generate command size = ceil(currSeed / N), seed = currSeed
-// this ensures that a deterministic sequence of generate commands are sent
-// there will be N objects of each size generated
 module.exports = function (seed, N) {
     N = N || 1;
     var size = Math.ceil(seed / N);
+    size = size % 200;
     return {size: size, seed: seed};
 };
