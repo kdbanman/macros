3d3fdcab66cce634346b9ecd8cc75776efcbc7e3
kdbanman <kdbanman@ualberta.ca>
2014-06-21 02:08:11 -0600

    improve TODO

--- test/stateHash/lib/storage.js
+++ test/stateHash/lib/storage.js
@@ -6,6 +6,7 @@ storage.store = function (data, fn)
 {
     //TODO this should dissect data according to validation spec
     //TODO this should slam the dissected data in a postgres instance
+    //TODO this should only store serializations if there is no stored seed OR if the stored seed disagrees on any hash
     fs.writeFile('tmp.storage', JSON.stringify(data) + "\n", {flag: 'a'}, fn);
 };
 
