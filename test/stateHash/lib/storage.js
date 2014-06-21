3c66d293d9dda7c950198e28da8861b9a728b0bd
kdbanman <kdbanman@ualberta.ca>
2014-06-21 13:23:28 -0600

    move write time measurement to storage module.  update with table proposals.

--- test/stateHash/lib/storage.js
+++ test/stateHash/lib/storage.js
@@ -4,10 +4,18 @@ var storage = {};
 
 storage.store = function (data, fn)
 {
+    var time_writing = Date.now();
+
+    
     //TODO this should dissect data according to validation spec
     //TODO this should slam the dissected data in a postgres instance
     //TODO this should only store serializations if there is no stored seed OR if the stored seed disagrees on any hash
     fs.writeFile('tmp.storage', JSON.stringify(data) + "\n", {flag: 'a'}, fn);
+
+
+    //TODO store time writing and other unique-per-report stuff in a "reports" table
+    //     and the hashes, serializations, in an objects table
+    time_writing = Date.now() - time_writing;
 };
 
 module.exports = storage;
