a0b08aa2c60057a7f637477a8a1bf76e13954257
kdbanman <kdbanman@ualberta.ca>
2014-06-25 23:30:24 -0600

    implement postgres storage

--- test/stateHash/lib/storage.js
+++ test/stateHash/lib/storage.js
@@ -33,11 +33,61 @@ storage.store = function (data, fn)
 
     //TODO move validation call and error handling here
     
-    fs.writeFile('tmp.storage', JSON.stringify(data) + "\n", {flag: 'a'}, fn);
+    // connect to postgres server
+    pg.connect("/tmp/.s.PGSQL.5432 stateHash", function(error, client, done) {
+        if (error) {
+            // handle db connection error by calling callback
+            fn(error);
+            
+            // truthy passed to done removes client from connection pool
+            done(true);
+        } else {
+            
+            // store everything but the (undetermined) time_writing in the 
+            // "reports" table
+            var queryValues = [data.size,
+                               data.seed,
+                               data.sent,
+                               data.time_generation,
+                               data.time_serialization,
+                               data.object,
+                               data.time_hashing_djb2,
+                               data.hash_djb2,
+                               data.time_hashing_sdbm,
+                               data.hash_sdbm,
+                               data.time_hashing_javaHashCode,
+                               data.hash_javaHashCode,
+                               data.time_hashing_crc32,
+                               data.hash_crc32,
+                               data.rtt,
+                               data.user_agent,
+                               data.connected_clients,
+                               data.time_writing];
 
+            var preparedQuery = {name: queryname,
+                                 text: queryText,
+                                 values: queryValues};
 
-    //TODO store everything except the TBD time_writing in the "reports" table
-    time_writing = Date.now() - time_writing;
+            client.query(preparedQuery, function(error, results) {
+                if (error) {
+                    // handle insertion error
+                    fn(error);
+
+                    // truthy passed to done removes client from connection pool
+                    done(true);
+                } else {
+                    // TODO use results.rows to get the report_id
+                    //console.log("DEBUG");
+                    //console.log(JSON.stringify(results.rows);
+                    // TODO save time_writing to report_id
+                    time_writing = Date.now() - time_writing;
+
+                    // return client to connection pool
+                    done();
+                }
+            });
+        }
+    });
 };
 
 module.exports = storage;
