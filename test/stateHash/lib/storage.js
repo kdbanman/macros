f87ba4921efa4f22a94cc7911ec2e30c71c110f0
kdbanman <kdbanman@ualberta.ca>
2014-06-25 22:21:25 -0600

    include prepared statement and update todo

--- test/stateHash/lib/storage.js
+++ test/stateHash/lib/storage.js
@@ -1,20 +1,40 @@
-var fs = require('fs');
+var pg = require('pg');
 
 var storage = {};
+
+var queryName = "insert report";
+var queryText = "INSERT INTO reports (" +
+                    "size, " +
+                    "seed, " +
+                    "sent, " +
+                    "time_generation, " +
+                    "time_serialization, " +
+                    "object, " +
+                    "time_hashing_djb2, " +
+                    "hash_djb2, " +
+                    "time_hashing_sdbm, " +
+                    "hash_sdbm, " +
+                    "time_hashing_javaHashCode, " +
+                    "hash_javaHashCode, " +
+                    "time_hashing_crc32, " +
+                    "hash_crc32, " +
+                    "rtt, " +
+                    "user_agent, " +
+                    "time_writing) " +
+                "VALUES " +
+                "($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17) "+
+                "RETURNING report_id;";
 
 storage.store = function (data, fn)
 {
     var time_writing = Date.now();
 
+    //TODO move validation call and error handling here
     
-    //TODO this should dissect data according to validation spec
-    //TODO this should slam the dissected data in a postgres instance
-    //TODO this should only store serializations if there is no stored seed OR if the stored seed disagrees on any hash
     fs.writeFile('tmp.storage', JSON.stringify(data) + "\n", {flag: 'a'}, fn);
 
 
-    //TODO store time writing and other unique-per-report stuff in a "reports" table
-    //     and the hashes, serializations, in an objects table
+    //TODO store everything except the TBD time_writing in the "reports" table
     time_writing = Date.now() - time_writing;
 };
 
